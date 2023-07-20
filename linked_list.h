fe9f7f5b7777b81829cbe5f83242dde8351cddd9
Beri Bahar <69582371+berrybahar@users.noreply.github.com>
2023-07-20 15:36:44 +0000

    Linked List

---
+++ linked_list.h
@@ -0,0 +1,14 @@
+typedef struct node {
+    void* data;
+    struct node* next;
+} Node;
+ 
+typedef struct list {
+    int size;
+    Node* head;
+} List;
+
+List* create_list();
+void add_to_list(List* list, void* data);
+void* remove_from_list(List* list);
+void free_list(List* list);
