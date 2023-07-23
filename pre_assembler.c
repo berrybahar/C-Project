a93f800b2758c6b848ad72afe63a1fa89bf20615
Beri Bahar <69582371+berrybahar@users.noreply.github.com>
2023-07-23 13:52:25 +0000

    opcode

--- pre_assembler.c
+++ pre_assembler.c
@@ -3,14 +3,14 @@
 FILE *macroSpreading(FILE *iofp)
 {
     List* macro_list = create_list();
-    FILE *fileAfterSpreadingMacros
+    FILE *fileAfterSpreadingMacros = fopen(fileExtensionChanger(), ".am");
     char line_in_file[MAX_LINE_LENGTH];
     int hasMacro = FALSE;
     
     while (!feof(iofp)) 
     {
        fread(line_in_file, MAX_LINE_LENGTH, 1, iofp);
-        
+       /*test*/
     }
     
     return fileAfterSpreadingMacros;
